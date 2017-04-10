6823efcb2363d858e4c7fc62ee2dc4ac9e12710d
Russell Gordon <rgordon@rsgc.on.ca>
2017-04-10 09:24:43 -0400

    Added CryptoSwift

--- TwoFactorAuthentication/ViewController.swift
+++ TwoFactorAuthentication/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CryptoSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("about to load view")
+        var message = "Hey, did this work?"
+        
+        print(message.md5())
+        
     }
 
     override func didReceiveMemoryWarning() {
